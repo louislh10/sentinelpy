4fffce1a3b93053290ada3de09383119d8e46eb6
Louis <louis.lehel@viacesi.fr>
2025-11-17 14:03:36 +0100

    extraction IP, date, IP Publique + Erreur = Suspect

--- generator_log.py
+++ generator_log.py
@@ -1,14 +1,29 @@
 messages = ["Failed password for invalid user", "Failed password for root from", "Accepted password for user bob from"] 
+ips = ["192.168.1.15", "10.0.0.42", "45.183.76.22", "203.0.113.55"]
 
 import time
 import random
+from datetime import datetime
+import ipaddress
+import re
 
-with open("test.log", "w") as f:
-    while True :
-        line = (random.choice(messages) + "\n")
-        f.write(line)
-        print(line)
-        f.seek(0, 2) # se positionner à la fin du fichier pour lire les nouvelles lignes
-        time.sleep(5)
+def is_private_ip(ip):
+    return ipaddress.ip_address(ip).is_private
+
+
+if __name__ == "__main__":
+    with open("test.log", "a") as f:
+        while True :
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            line = f"{timestamp} { (random.choice( messages) + "\n")}"
+
+            ip= random.choice(ips)
+            msg =random.choice(messages)
+            line = f"{timestamp} {msg} {ip}\n"
+            f.write(line)
+            print(line)
+            f.seek(0, 2) # se positionner à la fin du fichier pour lire les nouvelles lignes
+            time.sleep(2)
+    
 
 #écriture dans test log en temps réel
--- logs_analyzer.py
+++ logs_analyzer.py
@@ -1,17 +1,35 @@
 # système de détections d'intrusions 
 
 import time
+from generator_log import is_private_ip
+import re
+
 
 
 with open("test.log", "r") as f:
     f.seek(0, 2)
-    while True: # surveillance constante du fichier
-        line = f.readline()
-        if "Failed password" in line:
-            print(line)
-        else :
-            time.sleep(0.1)
+    while True: # surveillance constante du fichier test log
+        line=f.readline()
+        if not line : 
+            print("en attente de nouvelles lignes...")
+            time.sleep(2)
+        
             continue
+        if "Accepted password" in line :
+                continue 
+        
+        match = re.search(r'(\d{1,3}\.){3}\d{1,3}', line)
+        if match :
+             ip = match.group()
+        else:
+              continue  # aucune IP on passe à la ligne suivante
+            
+        if "Failed password" in line or "Invalid user" in line :
+            if not is_private_ip(ip): 
+                print("Adresse IP suspecte : ", ip)
+                
+
         
 
-         
+
+       
